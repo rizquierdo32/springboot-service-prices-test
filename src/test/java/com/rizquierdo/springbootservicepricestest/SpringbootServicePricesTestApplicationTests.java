992c7b8a68a3604368da7eb1972fb2e9632c58fd
rizquierdo <rizquierdo@isyc.com>
2023-12-21 17:15:33 +0100

    Inicio proyecto

---
+++ src/test/java/com/rizquierdo/springbootservicepricestest/SpringbootServicePricesTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rizquierdo.springbootservicepricestest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootServicePricesTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
